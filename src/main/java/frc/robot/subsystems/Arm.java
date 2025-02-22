f872b54aab1902043888cadf9dd82afa5d398a02
Jack Webb <webbjt07@gmail.com>
2025-02-22 16:02:55 -0500

    organized code and removed unused methods

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
@@ -16,19 +15,13 @@ import java.util.function.BooleanSupplier;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.MutAngle;
 import edu.wpi.first.units.measure.MutAngularAcceleration;
 import edu.wpi.first.units.measure.MutAngularVelocity;
 import edu.wpi.first.units.measure.MutVoltage;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -39,25 +32,21 @@ public class Arm extends SubsystemBase {
   private final TalonFX armMotor;
   private final MutVoltage armMotorVoltage;
   private final MutAngle armMotorAngle;
-  private final MutAngularVelocity armMotorVelo; 
-  private final MutAngularAcceleration armMotorAccel; 
+  private final MutAngularVelocity armMotorVelo;
+  private final MutAngularAcceleration armMotorAccel;
   private final SysIdRoutine armRoutine;
-  /** Creates a Arm */
-
-  //TODO:: Configure an offset for the arm motor so 0 in at the intake position
 
   public Arm() {
-    
     armMotor = new TalonFX(Constants.ArmConstants.ARM_MOTOR_ID);
-    //sysid
+    // sysid
     armMotorVoltage = Volts.mutable(0);
     armMotorAngle = Radians.mutable(0);
     armMotorVelo = RadiansPerSecond.mutable(0);
     armMotorAccel = RadiansPerSecondPerSecond.mutable(0);
-    
+
     armMotor.getConfigurator().apply(new SoftwareLimitSwitchConfigs().withForwardSoftLimitThreshold(Rotations.of(18))
-    .withReverseSoftLimitThreshold(Rotations.of(-.2)).withForwardSoftLimitEnable(true)
-    .withReverseSoftLimitEnable(true));
+        .withReverseSoftLimitThreshold(Rotations.of(-.2)).withForwardSoftLimitEnable(true)
+        .withReverseSoftLimitEnable(true));
     armMotor.setNeutralMode(NeutralModeValue.Brake);
 
     TalonFXConfiguration config = new TalonFXConfiguration();
@@ -65,63 +54,52 @@ public class Arm extends SubsystemBase {
     config.MotionMagic = Constants.ArmConstants.ARM_MOTION_CONFIGS;
     armMotor.getConfigurator().apply(config);
     armMotor.setNeutralMode(NeutralModeValue.Brake);
-    
-    
-    
+
     armRoutine = new SysIdRoutine(
-      new SysIdRoutine.Config(), 
-      new SysIdRoutine.Mechanism(
-        voltage -> {
-          armMotor.setVoltage(voltage.magnitude());
-        }, 
-        log -> {
-          log.motor("armMotor")
-          .voltage(armMotorVoltage.mut_replace(armMotor.getMotorVoltage().getValue()))
-          .angularVelocity(armMotorVelo.mut_replace(armMotor.getVelocity().getValue()))
-          .angularAcceleration(armMotorAccel.mut_replace(armMotor.getAcceleration().getValue()))
-          .angularPosition(armMotorAngle.mut_replace(armMotor.getPosition().getValue()));
-        },
-      this
-      )
-    );    
+        new SysIdRoutine.Config(),
+        new SysIdRoutine.Mechanism(
+            voltage -> {
+              armMotor.setVoltage(voltage.magnitude());
+            },
+            log -> {
+              log.motor("armMotor")
+                  .voltage(armMotorVoltage.mut_replace(armMotor.getMotorVoltage().getValue()))
+                  .angularVelocity(armMotorVelo.mut_replace(armMotor.getVelocity().getValue()))
+                  .angularAcceleration(armMotorAccel.mut_replace(armMotor.getAcceleration().getValue()))
+                  .angularPosition(armMotorAngle.mut_replace(armMotor.getPosition().getValue()));
+            },
+            this));
   }
+
   public Command armSysidQuasistatic(SysIdRoutine.Direction direction) {
     return armRoutine.quasistatic(direction);
-  }  
+  }
+
   public Command armSysidDynamic(SysIdRoutine.Direction direction) {
     return armRoutine.dynamic(direction);
   }
 
   public Command runToRotationsMagic(double setpointRotations) {
     MotionMagicVoltage request = new MotionMagicVoltage(armMotor.getPosition().getValueAsDouble());
-    return run(()-> armMotor.setControl(request.withPosition(setpointRotations)));
+    return run(() -> armMotor.setControl(request.withPosition(setpointRotations)));
   }
-  
+
   public double getMotionMagic() {
-  return armMotor.getMotionMagicIsRunning().getValueAsDouble();
+    return armMotor.getMotionMagicIsRunning().getValueAsDouble();
   }
-  
+
   public Command stopArm() {
-    return runOnce(()-> armMotor.setVoltage(0));
+    return runOnce(() -> armMotor.setVoltage(0));
   }
-  public BooleanSupplier canRaise(){
-    return () -> armMotor.getPosition().getValueAsDouble() > 4; 
+
+  public BooleanSupplier canRaise() {
+    return () -> armMotor.getPosition().getValueAsDouble() > 4;
   }
 
   public double armPosition() {
     return armMotor.getPosition().getValueAsDouble();
   }
 
-  public Command scrimageArmForward() {
-    return runOnce(() -> armMotor.set(.1));
-  }
-
-  public Command scrimageArmBackward() {
-    return runOnce(() -> armMotor.set(-0.1));
-  }
-  // runToPosition(10);
-  // runToPosition(20);
-  // runToPosition(30);
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
