3e0598bbc8c5a1505953063b8ad522c0643a7f27
skye <skyewells675@gmail.com>
2025-03-06 20:13:40 -0600

    tried to make auto work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -179,6 +180,18 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return chooser.getSelected();
+        return AutoBuilder.buildAuto("test");
+    }
+
+    public void autonomousInit(){
+        Command autoCommand = getAutonomousCommand();
+
+        if (autoCommand != null){
+            autoCommand.schedule();
+        }
+    }
+
+    public void robotPeriodic(){
+        CommandScheduler.getInstance().run();
     }
 }
