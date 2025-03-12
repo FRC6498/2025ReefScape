416b74877a7c6b9586d6579d32abad2692e8e440
skye <skyewells675@gmail.com>
2025-03-11 20:27:26 -0500

    added basic climber code todo: figure out how to best control climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,14 +4,34 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
-  public Climber() {}
+  private TalonFX climbMotor = new TalonFX(26);
+  public Climber() {
+
+    // todo get PID
+    // Slot0Configs slot0Config = new Slot0Configs();
+    // climbMotor.getConfigurator()
+
+  }
+
+  public Command runForward(){
+    return runOnce(()->climbMotor.set(.1));
+  }
+
+  public Command runReverse(){
+    return runOnce(()->climbMotor.set(-.1));
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("climber position", climbMotor.getPosition().getValueAsDouble());
   }
 }
