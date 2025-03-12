1e7638ee5a09d14d323e9c0d9597e9c94315aa7b
skye <skyewells675@gmail.com>
2025-03-11 19:48:36 -0500

    confirmed linear actuator class works added mor functionality

--- src/main/java/frc/robot/LinearActuator.java
+++ src/main/java/frc/robot/LinearActuator.java
@@ -1,4 +1,6 @@
+package frc.robot;
 import static edu.wpi.first.units.Units.Millimeter;
+import static edu.wpi.first.units.Units.Millimeters;
 
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.PWM;
@@ -12,9 +14,19 @@ public class LinearActuator extends PWM {
 
         lengthmm = length.in(Millimeter);
     }
+    
+    public LinearActuator(int channel, double length){
+        super(channel);
+
+        lengthmm = length;
+    }
 
     public void setLength(double setPos) {
         super.setSpeed( setPos / lengthmm * 2 - 1);
+    }
+
+    public void setLength(Distance setPos) {
+        setLength(setPos.in(Millimeters));
     }
 
 }
