a05d1408aaddec3828e7c3bbfcbdafdcc9f34dab
skye <skyewells675@gmail.com>
2025-03-16 13:06:06 -0400

    final commit

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -62,7 +62,7 @@ public class Climber extends SubsystemBase {
   }
 
   public Command reverseLatch(){
-    return runOnce(()->latchActuator.setLength(Inches.of(1.9)));
+    return runOnce(()->latchActuator.setLength(Inches.of(2.5)));
   }
 
   @Override
